a1ade9366c37717d7ab069131974c0c7287e442f
Gani Hasmegaj <ganhas20@htl-shkoder.com>
2025-02-18 12:37:57 +0100

    new changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,73 @@
 <script>
-  import { onMount } from "svelte";
-  let countries = [];
-
-  onMount(async () => {
-    const res = await fetch("https://restcountries.com/v3.1/all");
-    countries = await res.json();
-  });
-</script>
-
-<h1>Countries</h1>
-<ul>
-  {#each countries as country}
-    <li>
-      <a href={"/countries/" + country.cca3}>{country.name.common}</a>
-    </li>
-  {/each}
-</ul>
+    import { onMount } from "svelte";
+    let countries = [];
+  
+    onMount(async () => {
+      const res = await fetch("https://restcountries.com/v3.1/all");
+      countries = await res.json();
+    });
+  </script>
+  
+  <h1>Countries</h1>
+  <ul>
+    {#each countries as country}
+      <li>
+        <a href={"/countries/" + country.cca3}>{country.name.common}</a>
+      </li>
+    {/each}
+  </ul>
+  
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+  
+    * {
+      color: rgb(184, 5, 5);
+      font-family: 'Arial', sans-serif;
+      padding: 20px;
+    }
+  
+    h1 {
+      text-align: center;
+      font-size: 2.5rem;
+      margin-bottom: 20px;
+      font-weight: 600;
+      color: #1f95e4;
+    }
+  
+    ul {
+      list-style-type: none;
+      padding-left: 0;
+    }
+  
+    li {
+      margin: 10px 0;
+    }
+  
+    a {
+      color: #1815b9;
+      text-decoration: none;
+      font-size: 1.2rem;
+      transition: color 0.3s ease;
+    }
+  
+    a:hover {
+      color: #033e61;
+    }
+  
+    li:hover {
+      background-color: #3e6e6e;
+      border-radius: 5px;
+      padding: 5px 10px;
+    }
+  
+    li a {
+      display: block;
+      padding: 10px;
+      border-radius: 5px;
+    }
+  </style>
+  
